abc5f4808256e0f5d91e9d4a6b5fd40f806d060f
Mayur Vijay Bhat <mayappbhat@gmail.com>
2025-05-14 17:38:49 -0400

    Dependency Inversion Principle in Depth

--- Patterns/SOLID.py
+++ Patterns/SOLID.py
@@ -46,6 +46,51 @@ class PDFtoJPEGConvertor(FileConvertor):
 class UploadFiles(ABC):
     def upload_file(self, file):
         pass
+
+class DocumentService:
+    def __init__(self):
+        self.converter = PDFtoDOCXConvertor()
+        self.uploader = UploadFiles()
+
+    def process(self, file):
+        self.converter.convert(file)
+        self.uploader.upload_file(file)
 """
 Now, if any other type has to be added, just EXTEND the class to add another PDFtoTXT class.
 """
+
+#After 'D' - Dependency Inversion Principle
+"""
+High level modules should not be dependent on the lower level modules
+"""
+class FileConvertor(ABC):
+    def convert(self, file):
+        pass
+
+class PDFtoDOCXConvertor(FileConvertor):
+    def convert(self, file):
+        print("PDF to DOCX")
+
+class PDFtoJPEGConvertor(FileConvertor):
+    def convert(self, file):
+        print("PDF to JPEG")    
+
+class UploadFiles(ABC):
+    def upload_file(self, file):
+        pass
+
+class DocumentService:
+    def __init__(self, converter, uploader):
+        self.converter = converter
+        self.uploader = uploader
+
+    def process(self, file):
+        self.converter.convert(file)
+        self.uploader.upload_file(file)
+
+class Client:
+    pdf_to_doc = PDFtoDOCXConvertor()
+    uploader = UploadFiles()
+
+    service = DocumentService(pdf_to_doc, uploader)
+    service.process("Resume.pdf")
